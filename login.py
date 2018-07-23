eaff9cd1025076b6c40b4dcc44bf57f268432929
Clara Everett <everettclara8@gmail.com>
2018-07-23 07:45:31 -0700

    first edition

---
+++ login.py
@@ -0,0 +1,43 @@
+from google.appengine.api import users
+import webapp2
+#import jinja2
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        # [START user_details]
+        user = users.get_current_user()
+        if user:
+            nickname = user.nickname()
+            logout_url = users.create_logout_url('/')
+            greeting = 'Welcome, {}! (<a href="{}">sign out</a>)'.format(
+                nickname, logout_url)
+            #Redirect to Main Page
+            self.redirect('/homePage')
+
+        else:
+            login_url = users.create_login_url('/')
+            greeting = '<a href="{}">Sign in</a>'.format(login_url)
+        # [END user_details]
+        self.response.write(
+            '<html><body>{}</body></html>'.format(greeting))
+
+class AdminPage(webapp2.RequestHandler):
+    def get(self):
+        user = users.get_current_user()
+        if user:
+            if users.is_current_user_admin():
+                self.response.write('You are an administrator.')
+            else:
+                self.response.write('You are not an administrator.')
+        else:
+            self.response.write('You are not logged in.')
+
+class homePage(webapp2.RequestHandler):
+    def get(self):
+        print 'we need to work on this page!'
+
+app = webapp2.WSGIApplication([
+    ('/', MainPage),
+    ('/homePage', homePage),
+    ('/admin', AdminPage)
+], debug=True)
