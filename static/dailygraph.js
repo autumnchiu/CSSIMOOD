fe4035b6cc2545df2c4955aa0ca5fbf0da513df1
Clara Everett <everettclara8@gmail.com>
2018-07-25 09:22:10 -0700

    edited the graph: moved js into html and made data and label in python

--- static/dailygraph.js
+++ static/dailygraph.js
@@ -5,7 +5,7 @@ new Chart(document.getElementById("line-chart"), {
     labels: [1500,1600,1700,1750,1800,1850,1900,1950,1999,2050],
     datasets: [{
         //should be numeric value for emotion
-        data: [6,3,2,2,7,26,82,172,312,433],
+        data: [-1,3,-15,2,7,26,82,172,312,433],
         // label: "North America",
         borderColor: "#4BC4AE",
         fill: false
