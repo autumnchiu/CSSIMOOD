2ddc8c6bcf97f29ba7ace5b469211cb7977ec938
Clara Everett <everettclara8@gmail.com>
2018-07-25 08:36:33 -0700

    added new html and js file for dailygraph

---
+++ static/dailygraph.js
@@ -0,0 +1,21 @@
+new Chart(document.getElementById("line-chart"), {
+  type: 'line',
+  data: {
+    // x-axis, will become timestamp
+    labels: [1500,1600,1700,1750,1800,1850,1900,1950,1999,2050],
+    datasets: [{
+        //should be numeric value for emotion
+        data: [6,3,2,2,7,26,82,172,312,433],
+        // label: "North America",
+        borderColor: "#4BC4AE",
+        fill: false
+      }
+    ]
+  },
+  options: {
+    title: {
+      display: true,
+      text: 'A graph of your emotions'
+    }
+  }
+});
