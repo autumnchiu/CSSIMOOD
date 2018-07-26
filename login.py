faff1bbfeac8da2e4b6f87e8149f245a14e1ea92
Clara Everett <everettclara8@gmail.com>
2018-07-26 07:07:12 -0700

    added the new main page, edited the buttons and their colors!!

--- login.py
+++ login.py
@@ -15,44 +15,59 @@ JINJA_ENV = jinja2.Environment(
 
 DESCRIPTION = {
 'I laughed': {'feeling': 'joyful',
-              'color': '#EA76FF',
-			  'index': '1'},
+              'color': '#02BCB8',
+			  'index': 1},
 'I feel energetic': {'feeling':'joyful',
-                     'color': '#FF43CA',
-					 'index': '1'},
+                     'color': '#05ABC0',
+					 'index': 1},
 'I smiled': {'feeling': 'joyful',
-             'color':'#FF184D',
-			 'index': '1'},
+             'color':'#025D9C',
+			 'index': 1},
 'I feel confident': {'feeling': 'powerful',
-                     'color': '#FFCB33'},
+                     'color': '#97D4D1',
+					 'index': 1},
 'I feel appreciated': {'feeling': 'powerful',
-                        'color':'#DA841D'},
+                        'color':'#6BB9C6',
+						'index': 1},
 'I feel valuable': {'feeling': 'powerful',
-                        'color':'#FF4D09'},
+                        'color':'#5D4E4F',
+						'index': 1},
 'I am thankful': {'feeling': 'peaceful',
-                  'color': '#ACFF3A'},
+                  'color': '#3C9BD1',
+				  'index': 1},
 'I am content': {'feeling':'peaceful',
-                 'color':'#8BDA33'},
+                 'color':'#486476',
+				 'index': 1},
 'I feel relaxed': {'feeling': 'peaceful',
-                   'color':'#62C74A'},
+                   'color':'#B4A893',
+				   'index': 1},
 'I feel burnt out': {'feeling':'sad',
-                     'color':'#91F6FF'},
+                     'color':'#F1C131',
+					 'index': -1},
 'I am homesick': {'feeling':'sad',
-                    'color':'#38FFAF'},
+                    'color':'#F1DD48',
+					'index': -1},
 'I feel like an imposter': {'feeling':'sad',
-                            'color':'#61FAFF'},
+                            'color':'#F1AD48',
+							'index': -1},
 'I am annoyed': {'feeling':'mad',
-                 'color':'#9890FF'},
+                 'color':'#F1AD7C',
+				 'index': -1},
 'I am cranky': {'feeling':'mad',
-                'color': '#817BD9'},
+                'color': '#F1ADA1',
+				'index': -1},
 'I feel betrayed': {'feeling': 'mad',
-                    'color':'#966FC4'},
+                    'color':'#EDADC3',
+					'index': -1},
 'I am embarassed': {'feeling':'scared',
-                    'color':'#96485C'},
+                    'color':'#FBDDCE',
+					'index': -1},
 'I feel vulnerable': {'feeling':'scared',
-                      'color':'#BF4C47'},
+                      'color':'#AED8BD',
+					  'index': -1},
 'I feel helpless': {'feeling': 'scared',
-                    'color':'#BF4047'},
+                    'color':'#BED85E',
+					'index': -1},
 }
 
 class Feelings(ndb.Model):
@@ -95,65 +110,6 @@ class homePage(webapp2.RequestHandler):
         logout_url = users.create_logout_url('/')
         signout = 'Sign Out'
         link = logout_url
-
-        # description = {
-        # 'I laughed': {'feeling': 'joyful',
-        #               'color': '#EA76FF'},
-        # 'I feel energetic': {'feeling':'joyful',
-        #                      'color': '#FF43CA'},
-        # 'I smiled': {'feeling': 'joyful',
-        #              'color':'#FF184D'},
-        # 'I feel confident': {'feeling': 'powerful',
-        #                      'color': '#FFCB33'},
-        # 'I feel appreciated': {'feeling': 'powerful',
-        #                         'color':'#DA841D'},
-        # 'I feel valuable': {'feeling': 'powerful',
-        #                         'color':'#FF4D09'},
-        # 'I am thankful': {'feeling': 'peaceful',
-        #                   'color': '#ACFF3A'},
-        # 'I am content': {'feeling':'peaceful',
-        #                  'color':'#8BDA33'},
-        # 'I feel relaxed': {'feeling': 'peaceful',
-        #                    'color':'#62C74A'},
-        # 'I feel burnt out': {'feeling':'sad',
-        #                      'color':'#91F6FF'},
-        # 'I am homesick': {'feeling':'sad',
-        #                     'color':'#38FFAF'},
-        # 'I feel like an imposter': {'feeling':'sad',
-        #                             'color':'#61FAFF'},
-        # 'I am annoyed': {'feeling':'mad',
-        #                  'color':'#9890FF'},
-        # 'I am cranky': {'feeling':'mad',
-        #                 'color': '#817BD9'},
-        # 'I feel betrayed': {'feeling': 'mad',
-        #                     'color':'#966FC4'},
-        # 'I am embarassed': {'feeling':'scared',
-        #                     'color':'#96485C'},
-        # 'I feel vulnerable': {'feeling':'scared',
-        #                       'color':'#BF4C47'},
-        # 'I feel helpless': {'feeling': 'scared',
-        #                     'color':'#BF4047'},
-
-
-        # }
-        # params['emotions'] = [
-        #     'I laughed',
-        #     'I feel confident',
-        #     'I feel energetic',
-        #     'I smiled',
-        #     'I am thankful',
-        #     'I am annoyed',
-        #     'I feel like an imposter',
-        #     'I want to sleep',
-        #     'I am cranky',
-        #     'I am homesick',
-        #     'I feel vulnerable',
-        #     'I feel burnt out',
-        #     'I am jealous',
-        #     'I feel betrayed',
-        #     'I feel weak',
-        # ]
-
         self.response.write(content.render(emotions=DESCRIPTION, signout=signout, link=link))
 
 
@@ -164,26 +120,11 @@ class EmotionHandler(webapp2.RequestHandler):
         emotionpage = JINJA_ENV.get_template('templates/emotionpage.html')
         self.response.write(emotionpage.render(emotion=my_emotion, color = DESCRIPTION[my_emotion]['color']))
 
-# class CalendarHandler(webapp2.RequestHandler):
-# 	def get(self):
-# 		calendar_template = JINJA_ENV.get_template('templates/dailylog.html')
-# 		var = {
-# 		'month': 'July',
-# 		'year': '2018',
-# 		'weeks_in_month': [
-# 		[1,2,3,4,5,6,7],
-# 		[8,9,10,11,12,13,14],
-# 		[15,16,17,18,19,20,21],
-# 		[22,23,24,25,26,27,28],
-# 		[29,30,31]
-# 		]
-# 		}
-# 		self.response.write(calendar_template.render(var))
-
 class aboutpageHandler(webapp2.RequestHandler):
     def get(self):
         about_template = JINJA_ENV.get_template('templates/about.html')
         self.response.write(about_template.render())
+
 
 class dailyLog(webapp2.RequestHandler):
     def post(self):
@@ -206,7 +147,6 @@ class dailyLog(webapp2.RequestHandler):
         print date
 
         tableData = Feelings.query(
-
     		ndb.AND(Feelings.chosen_time >= date,
             Feelings.chosen_time < date + datetime.timedelta(days=1), Feelings.user == users.get_current_user().user_id())).order(Feelings.chosen_time)
         self.response.write(table_template.render(tableData = tableData))
@@ -218,15 +158,20 @@ class StyleHandler(webapp2.RequestHandler):
 
 class dailyGraphHandler(webapp2.RequestHandler):
     def get(self):
-        xAxis= []
+        xAxis = []
+        yAxis = []
+        my_emotion = self.request.get('my_emotion')
         allTimes = Feelings.query()
+        index = DESCRIPTION[my_emotion]['index']
         for time in sorted(allTimes, key = lambda t: t.chosen_time):
-            print(time.chosen_time)
-            print(datetime.datetime.strfttime(time.chosen_time, '%H:%M'))
-            xAxis.append(datetime.datetime.strfttime(time.chosen_time, '%H:%M'))
-        data = [-1,3,-15,2,7,26,82,172,312,433]
+            #print(time.chosen_time)
+            print(datetime.datetime.strftime(time.chosen_time, '%H:%M'))
+            #print(chosen_intensity)
+            xAxis.append(datetime.datetime.strftime(time.chosen_time, '%H:%M'))
+            yAxis.append(index*chosen_intensity)
+        #data = [-1,3,-15,2,7,26,82,172,312,433]
         dailygraph = JINJA_ENV.get_template('templates/dailygraph.html')
-        self.response.write(dailygraph.render(xAxis = ','.join(['"%s"'% x for x in xAxis]), data = data))
+        self.response.write(dailygraph.render(xAxis = ','.join(['"%s"'% x for x in xAxis]), yAxis=yAxis))
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
